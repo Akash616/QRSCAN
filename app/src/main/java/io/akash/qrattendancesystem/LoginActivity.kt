0f6f3c9d46f3d1f94e4c009fe7b98f21be9f0f26
Akash Gupta <61616akash@gmail.com>
2026-04-06 08:53:28 +0530

    fixes

--- app/src/main/java/io/akash/qrattendancesystem/LoginActivity.kt
+++ app/src/main/java/io/akash/qrattendancesystem/LoginActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import io.akash.qrattendancesystem.databinding.ActivityLoginBinding
 
@@ -42,9 +44,18 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
+        enableEdgeToEdge()
+
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Insets handling (optional now)
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content)) { view, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.setPadding(0, systemBars.top, 0, systemBars.bottom)
+            insets
+        }*/
+
 
         // Teacher button click
         binding.btnTeacher.setOnClickListener {
